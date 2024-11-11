f918eabc113bdc3a572cc4e2c24a99a446a1e1ee
taehwankang97 <kwth2002hs@naver.com>
2024-11-11 15:02:42 +0900

    detail 기본 세팅

--- src/main/java/phantom/books/finalProject/searchBookPage/controller/SearchBookPageController.java
+++ src/main/java/phantom/books/finalProject/searchBookPage/controller/SearchBookPageController.java
@@ -1,10 +1,29 @@
 package phantom.books.finalProject.searchBookPage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("SearchBookPage")
+@RequestMapping("searchBookPage")
 public class SearchBookPageController {
 
+//	검색후 페이지
+	@GetMapping("searchBook")
+	public String searchBook() {
+		
+		
+		
+		return "searchBook/searchBook";
+	}
+	
+	
+// 상세조회
+	@GetMapping("bookDetail")
+	public String bookDetail() {
+		
+		
+		return "searchBookPage/bookDetail";
+	}
+	
 }
--- src/main/java/phantom/books/finalProject/searchBookPage/service/SearchBookPageServiceImpl.java
+++ src/main/java/phantom/books/finalProject/searchBookPage/service/SearchBookPageServiceImpl.java
@@ -1,8 +1,13 @@
 package phantom.books.finalProject.searchBookPage.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class SearchBookPageServiceImpl implements SearchBookPageService{
 
+	
+	
+	
+	
 }
