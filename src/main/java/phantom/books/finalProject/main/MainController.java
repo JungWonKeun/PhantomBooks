5ebbc278e0c31300becc5d72bffd2c8eeb583abc
MAX-G <go1991woo@naver.com>
2024-11-12 11:31:45 +0900

    패키지 경로 수정

--- src/main/java/phantom/books/finalProject/main/MainController.java
+++ src/main/java/phantom/books/finalProject/main/MainController.java
@@ -1,4 +1,4 @@
-package phantom.books.finalProject.main.controller;
+package phantom.books.finalProject.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
