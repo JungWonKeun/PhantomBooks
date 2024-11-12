6aeeed4179e994c57d0e04f4e646a5bd19f579ec
Daehyun009 <hanmir009@naver.com>
2024-11-12 11:20:40 +0900

    20241112 로그인 백엔드 코드 작성 완료

--- src/main/java/phantom/books/finalProject/member/service/MemberService.java
+++ src/main/java/phantom/books/finalProject/member/service/MemberService.java
@@ -4,6 +4,11 @@ import phantom.books.finalProject.member.dto.Member;
 
 public interface MemberService {
 
+	/** 로그인 서비스
+	 * @param memberId
+	 * @param memberPw
+	 * @return loginMember 또는 null(email 또는 pw 불일치)
+	 */
 	Member login(String memberId, String memberPw);
 
 
