e214d0bb354575e7029c2cca0a501f438e66eabf
taehwankang97 <kwth2002hs@naver.com>
2024-11-13 17:45:21 +0900

    일단 다 못했지만 올립니다. 확인 부탁 드립니다

--- src/main/java/phantom/books/finalProject/searchBookPage/mapper/SearchBookPageMapper.java
+++ src/main/java/phantom/books/finalProject/searchBookPage/mapper/SearchBookPageMapper.java
@@ -12,7 +12,11 @@ public interface SearchBookPageMapper {
 	// 책 전체 조회
 	List<Book> allBook ();
 
+	// 제목의 책 조회
+	List<Book> searchBooksByTitle(String query);
+	
 	// 책 상세 조회
 	Book bookDetail(int bookNo);
+
 
 }
