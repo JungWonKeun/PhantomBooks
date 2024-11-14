526396c386d7b210e5a48bab940414c15ae02bca
yukpoyum <anokata@naver.com>
2024-11-14 11:23:44 +0900

    결제기능 + mainjs추가

--- src/main/java/phantom/books/finalProject/order/controller/OrderController.java
+++ src/main/java/phantom/books/finalProject/order/controller/OrderController.java
@@ -1,11 +1,15 @@
 package phantom.books.finalProject.order.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import phantom.books.finalProject.order.dto.OrderDto;
 import phantom.books.finalProject.order.service.OrderService;
 
 @Slf4j
@@ -16,12 +20,25 @@ import phantom.books.finalProject.order.service.OrderService;
 public class OrderController {
 	
 	private final OrderService service;
-	
-	
-	 @RequestMapping("")
-	 public String orderPage() {
-	     return "order/order";
-	 }
+
+	@RequestMapping("")
+	public String orderPage() {
+	    return "order/order";
+	}
 	 
-	
+	@GetMapping("/afterOrder")
+	public String afterOrder(
+			@RequestParam("orderId") String orderId, 
+			Model model) {
+	    OrderDto order = service.findOrderByOrderId(orderId);
+
+	    if (order == null) {
+	        model.addAttribute("errorMessage", "주문 정보를 찾을 수 없습니다.");
+	        return "error/error"; // 에러 페이지로 이동
+	    }
+
+	    model.addAttribute("order", order);
+	    return "order/afterOrder"; 
+	}
 }
+
