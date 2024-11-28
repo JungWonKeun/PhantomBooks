91b793a72d7d8c2138f6152138775d81e7b86dc3
Daehyun009 <hanmir009@naver.com>
2024-11-28 18:19:19 +0900

    20241128 아이디 비밀번호 찾기

--- src/main/java/phantom/books/finalProject/member/service/MemberService.java
+++ src/main/java/phantom/books/finalProject/member/service/MemberService.java
@@ -1,5 +1,7 @@
 package phantom.books.finalProject.member.service;
 
+import java.util.List;
+
 import phantom.books.finalProject.member.dto.Member;
 
 public interface MemberService {
@@ -29,7 +31,34 @@ public interface MemberService {
 
 
 
+	/** 다시 보지 않기를 체크한 회원번호에 맞는 회원의 categoryYn를 Y로 변경
+	 * @param memberNo
+	 */
 	void updateCategoryYn(int memberNo);
+
+
+
+	/** 전화번호로 아이디 찾기
+	 * @param telNo
+	 * @return
+	 */
+	List<String> findIdByTelNo(String telNo);
+
+
+
+	/** 아이디와 전화번호가 일치하는 회원 확인
+	 * @param telNo
+	 * @param memberId
+	 */
+	void checkIdAndTel(String telNo, String memberId);
+
+
+	
+	/** 비밀번호 변경
+	 * @param memberId
+	 * @param memberPw
+	 */
+	void updatePassword(String memberId, String memberPw);
 
 
 }
