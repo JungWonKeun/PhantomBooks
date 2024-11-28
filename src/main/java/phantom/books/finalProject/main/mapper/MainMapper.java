9b2de0a7c1cae82b9f7513aa8ecc7d5bfefa3a72
MAX-G <go1991woo@naver.com>
2024-11-28 20:21:30 +0900

    241128 메인페이지 구상도 정리 완료 // 섹션별로 메서드 내 LIST 내용 정리 후 모델 추가하여 기능에 맞게 구현하기

--- src/main/java/phantom/books/finalProject/main/mapper/MainMapper.java
+++ src/main/java/phantom/books/finalProject/main/mapper/MainMapper.java
@@ -1,5 +1,18 @@
 package phantom.books.finalProject.main.mapper;
 
+
+import org.apache.ibatis.annotations.Mapper;
+
+import phantom.books.finalProject.searchBookPage.dto.Book;
+
+import java.util.List;
+
+@Mapper
 public interface MainMapper {
 
+    List<Book> getDailyRecommendedBooks();
+
+    List<Book> getBooksByUserPreference(int userId);
+
+    List<Book> getBestsellerBooks();
 }
--- src/main/java/phantom/books/finalProject/main/service/MainServiceImpl.java
+++ src/main/java/phantom/books/finalProject/main/service/MainServiceImpl.java
@@ -1,5 +1,28 @@
 package phantom.books.finalProject.main.service;
 
-public class MainServiceImpl {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import phantom.books.finalProject.main.mapper.MainMapper;
+import phantom.books.finalProject.searchBookPage.dto.Book;
+
+@Service
+@RequiredArgsConstructor
+public class MainServiceImpl implements MainService {
+	
+	private final MainMapper mapper;
+	@Override
+	public List<Book> getBestsellerBooks() {
+		return mapper.getBestsellerBooks();
+	}
+	@Override
+	public List<Book> getBooksByUserPreference(int memberNo) {
+		return mapper.getBooksByUserPreference(memberNo);
+	}
+	@Override
+	public List<Book> getDailyRecommendedBooks() {
+		return mapper.getDailyRecommendedBooks();
+	}
 }
