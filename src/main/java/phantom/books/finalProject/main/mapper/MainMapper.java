a6d96b7296cffba177db1642cbe56e2e5438fe69
DESKTOP-A6IJ216\user <go1991woo@naver.com>
2024-11-28 22:20:44 +0900

    오버라이딩 누락으로 재커밋

--- src/main/java/phantom/books/finalProject/main/mapper/MainMapper.java
+++ src/main/java/phantom/books/finalProject/main/mapper/MainMapper.java
@@ -3,6 +3,7 @@ package phantom.books.finalProject.main.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import phantom.books.finalProject.order.dto.OrderBookDto;
 import phantom.books.finalProject.searchBookPage.dto.Book;
 
 import java.util.List;
@@ -15,4 +16,6 @@ public interface MainMapper {
     List<Book> getBooksByUserPreference(int userId);
 
     List<Book> getBestsellerBooks();
+
+		List<OrderBookDto> getDailyRecommendedList();
 }
