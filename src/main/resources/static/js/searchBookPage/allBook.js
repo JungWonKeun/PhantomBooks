303e9022e403ee425ec53cd8bb32401eb8964fc4
TAEBAN\kwth1 <kwth2002hs@naver.com>
2024-12-05 02:02:36 +0900

    찜 버튼

--- src/main/resources/static/js/searchBookPage/allBook.js
+++ src/main/resources/static/js/searchBookPage/allBook.js
@@ -762,10 +762,11 @@ function singleWishListBtn(button) {
         .then(data => {
             if (data === "추가 성공") {
                 alert(`"${bookTitle}"을(를) 찜 목록에 추가했습니다.`);
+                location.reload();
             } else {
                if(confirm(`이미 찜한 책입니다.
                 \n"${bookTitle}"를 삭제 하시겠습니까?`)){   
-
+                    
                 fetch(`/searchBookPage/deleteWishlist`, {
                     method: 'DELETE',
                     headers: {
@@ -782,6 +783,7 @@ function singleWishListBtn(button) {
                     .then(data => {
                     if (data === "삭제 성공") {
                         alert(`"${bookTitle}"을(를) 찜 목록에서 삭제했습니다.`);
+                        location.reload();
                     }
                     })
                     .catch(error => {
