ffc9f38fcc5eaa44c37df1b2db7342fb8de80ebf
yukpoyum <anokata@naver.com>
2024-12-05 20:30:43 +0900

    adminSales Css 수정 완료

--- src/main/resources/static/js/admin/adminSales.js
+++ src/main/resources/static/js/admin/adminSales.js
@@ -14,83 +14,132 @@ const listUp = (cp, sort, term, date, text) => {
 
   salesRank.innerHTML = '';
 
-  fetch("/admin/sales/salesList?cp="+cp+"&sort="+sort + "&term="+term+"&date="+date + "&text=" +text)
-  .then(response => {
-    if(response.ok) return response.json();
-    throw new Error("조회실패");
-  })
-  .then(map => {
-    console.log(map);
-
-    const pagination = map.pagination;
-    const salesList = map.salesList;
-
-    salesList.forEach(sales => {
-
-      const tr = document.createElement("tr");
-
-      const th1 = document.createElement("th");
-      th1.append(sales.bookNo);
-
-      const th2 = document.createElement("td");
-      th2.append(sales.bookTitle);
-
-      const th4 = document.createElement("td");
-      th4.append(sales.bookCount);
-
-      const th5 = document.createElement("td");
-      th5.append(sales.orderPrice);
-
-      tr.append(th1, th2, th4, th5);
-      
-      salesRank.append(tr);
+  fetch("/admin/sales/salesList?cp=" + cp + "&sort=" + sort + "&term=" + term + "&date=" + date + "&text=" + text)
+    .then(response => {
+      if (response.ok) return response.json();
+      throw new Error("조회실패");
     })
-    const sales = document.querySelector(".sales");
+    .then(map => {
+      console.log(map);
 
+      const pagination = map.pagination;
+      const salesList = map.salesList;
+
+      const formatPrice = (price) => {
+        return price.toLocaleString();
+      };
+
+      // 천 단위 콤마 추가 함수
+    
+
+      salesList.forEach(sales => {
+
+        const tr = document.createElement("tr");
+
+        const th1 = document.createElement("td");
+        th1.append(sales.bookNo);
+
+        const th2 = document.createElement("td");
+        th2.append(sales.bookTitle);
+
+        const th4 = document.createElement("td");
+        th4.append(sales.bookCount);
+
+        const th5 = document.createElement("td");
+        th5.append(formatPrice(sales.orderPrice));
+
+        tr.append(th1, th2, th4, th5);
+
+        salesRank.append(tr);
+      })
+      const sales = document.querySelector(".sales");
+
+      /*  금액에 천 단위로 콤마를 추가 */ 
+      
       sales.innerHTML = '';
 
+      /* 글씨 css 다르게 적용하기 위해 추가로 작성함 */
+      // 첫 번째 정보 div 생성
       const div1 = document.createElement("div");
-      const div2 = document.createElement("div");
-      const div3 = document.createElement("div");
-      
-      div1.innerHTML = "기간 내 판매 금액 : " + map.totalOrderPrice +"원";
-      div2.innerHTML = "기간 내 발주금액 : "  + map.totalRequestPrice + "원";
-      div3.innerHTML = "순 수익 : " + map.salesPrice + "원";
+
+      const spanSalesTitle = document.createElement("span");
+      spanSalesTitle.classList.add("sales-title");
+      spanSalesTitle.innerText = "기간 내 판매 금액";
+
+      const spanSalesCount = document.createElement("span");
+      spanSalesCount.classList.add("sales-count");
+      spanSalesCount.innerText = formatPrice(map.totalOrderPrice) + " 원";
+
+      div1.appendChild(spanSalesTitle);
+      div1.appendChild(document.createElement("br"));
+      div1.appendChild(spanSalesCount);
 
       sales.appendChild(div1);
+
+      // 두 번째 정보 div 생성
+      const div2 = document.createElement("div");
+
+      const spanRequestTitle = document.createElement("span");
+      spanRequestTitle.classList.add("sales-title");
+      spanRequestTitle.innerText = "기간 내 발주 금액";
+
+      const spanRequestCount = document.createElement("span");
+      spanRequestCount.classList.add("sales-count");
+      spanRequestCount.innerText = formatPrice(map.totalRequestPrice) + " 원";
+
+      div2.appendChild(spanRequestTitle);
+      div2.appendChild(document.createElement("br"));
+      div2.appendChild(spanRequestCount);
+
       sales.appendChild(div2);
+
+      // 세 번째 정보 div 생성
+      const div3 = document.createElement("div");
+
+      const spanProfitTitle = document.createElement("span");
+      spanProfitTitle.classList.add("sales-title");
+      spanProfitTitle.innerText = "순수익";
+
+      const spanProfitCount = document.createElement("span");
+      spanProfitCount.classList.add("sales-count");
+      spanProfitCount.innerText = formatPrice(map.salesPrice) + " 원";
+
+      div3.appendChild(spanProfitTitle);
+      div3.appendChild(document.createElement("br"));
+      div3.appendChild(spanProfitCount);
+
       sales.appendChild(div3);
 
-    // 페이지네이션 출력
-    const pg = document.querySelector('.pagination');
-    pg.innerHTML = '';
-    
-    // pagication 구조 분해
-    const {startPage, endPage, currentPage, prevPage, nextPage, maxPage} = pagination;
+      // 페이지네이션 출력
+      const pg = document.querySelector('.pagination');
+      pg.innerHTML = '';
 
-    // 버튼 생성 + 화면 추가 함수
-    const createPageBtn = (page, text) => {
-      const a = document.createElement('a');
-      a.textContent = text;
-      a.dataset.page = page;
+      // pagication 구조 분해
+      const { startPage, endPage, currentPage, prevPage, nextPage, maxPage } = pagination;
 
-      if(!isNaN(Number(text)) &&  page == currentPage) a.classList.add('current');
-      pg.append(a);
-    }
+      // 버튼 생성 + 화면 추가 함수
+      const createPageBtn = (page, text) => {
+        const a = document.createElement('a');
+        a.textContent = text;
+        a.dataset.page = page;
 
-    createPageBtn(1, '처음');
-    createPageBtn(prevPage, '이전');
+        if (!isNaN(Number(text)) && page == currentPage) a.classList.add('current');
+        pg.append(a);
+      }
 
-    for(let i = startPage; i <= endPage; i++) {
-      createPageBtn(i, i);
-    }
+      createPageBtn(1, '처음');
+      createPageBtn(prevPage, '이전');
 
-    createPageBtn(nextPage, '다음');
-    createPageBtn(maxPage, '마지막');
+      for (let i = startPage; i <= endPage; i++) {
+        createPageBtn(i, i);
+      }
 
-    // 페이지네이션 클릭 이벤트 추가
-    paginationAddEvent();
-  })
+      createPageBtn(nextPage, '다음');
+      createPageBtn(maxPage, '마지막');
+
+      // 페이지네이션 클릭 이벤트 추가
+      paginationAddEvent();
+    })
 
 }
 
@@ -115,7 +164,7 @@ buttons.forEach(button => {
     buttons.forEach(btn => btn.classList.remove("active"));
 
     button.classList.add("active");
-    
+
     if (!button.classList.contains("dateSelect")) {
       date.classList.add("hidden");
     }
@@ -133,7 +182,7 @@ week.addEventListener("click", () => {
 month.addEventListener("click", () => {
   term = "month";
   listUp(1, sortSelect.value, term);
-  
+
   chartData(1, sortSelect.value, term);
 });
 sixMonth.addEventListener("click", () => {
@@ -145,13 +194,13 @@ sixMonth.addEventListener("click", () => {
 });
 
 dateSelect.addEventListener("click", () => {
-  
+
   date.classList.remove('hidden');
 
-  date.addEventListener("change", ()=>{
+  date.addEventListener("change", () => {
 
     term = "dateSelect";
-    
+
     chartData(1, sortSelect.value, term, date.value);
 
     listUp(1, sortSelect.value, term, date.value);
@@ -169,7 +218,7 @@ sortSelect.addEventListener("change", () => {
 
   term = '';
 
-  buttons.forEach(button => button.classList.remove('active')); 
+  buttons.forEach(button => button.classList.remove('active'));
   searchText.value = '';
   searchText.classList.add('hidden');
 
@@ -180,7 +229,7 @@ sortSelect.addEventListener("change", () => {
     chartData(1, selectedSort, termSelect.value, date.value);
 
     listUp(1, selectedSort, term.value, date.value);
-  } else{
+  } else {
     termSelect.classList.add('hidden');
     searchText.classList.remove("hidden");
 
@@ -189,7 +238,7 @@ sortSelect.addEventListener("change", () => {
     //   text = searchText.value;
 
     //   listUp(1, selectedSort, term.value, date.value, text);
-      
+
     // })
     // const searchBtn = document.querySelector("#searchBtn");
 
@@ -197,25 +246,25 @@ sortSelect.addEventListener("change", () => {
     //   if(myChart !== null){
     //     myChart.destroy();
     //   }
-  
+
     //   chartData(1, selectedSort, term.value, date.value, text);
     // })
   }
 });
 
 searchText.addEventListener("input", () => {
-  const selectedSort =  sortSelect.value;
+  const selectedSort = sortSelect.value;
   text = searchText.value;
 
   listUp(1, selectedSort, term.value, date.value, text);
-  
+
 })
 
 const searchBtn = document.querySelector("#searchBtn");
 
 searchBtn.addEventListener("click", () => {
 
-  const selectedSort =  sortSelect.value;
+  const selectedSort = sortSelect.value;
   chartData(1, selectedSort, term.value, date.value, text);
 })
 
@@ -229,8 +278,8 @@ const paginationAddEvent = () => {
   pagination.forEach(a => {
     a.addEventListener('click', (e) => {
 
-      if(a.classList.contains('current')) return;
-      
+      if (a.classList.contains('current')) return;
+
       const cp = e.target.dataset.page;
       listUp(cp, sortSelect.value, termSelect.value, dateSelect.value, searchText.value);
     });
@@ -250,91 +299,93 @@ const chartData = (cp, sort, term, date, text) => {
   let orderPrice = new Array();
   let requestPrice = new Array();
 
-  fetch("/admin/sales/chartData?cp="+cp+ "&sort=" +sort + "&term=" +term+ "&date=" +date + "&text=" +text)
-  .then(response => {if(response.ok) return response.json();})
-  .then(list => {if(Array.isArray(list) && list.length != 0) {
-    console.log(list);
-    
-    if(myChart != null){
-      myChart.destroy();
-    }
-    // forEach로 list의 값을 newArray에 추가
-    list.forEach(chart => {
-      orderDate.push(chart.orderDate);
-      orderCount.push(chart.orderCount);
-      orderPrice.push(chart.orderPrice);
-      requestPrice.push(chart.requestPrice);
-    });
-    console.log(orderDate);
-    console.log(orderCount);
-    console.log(orderPrice);
-    console.log(requestPrice);
+  fetch("/admin/sales/chartData?cp=" + cp + "&sort=" + sort + "&term=" + term + "&date=" + date + "&text=" + text)
+    .then(response => { if (response.ok) return response.json(); })
+    .then(list => {
+      if (Array.isArray(list) && list.length != 0) {
+        console.log(list);
 
-
-    myChart = new Chart(document.querySelector("#myChart").getContext('2d'), {
-      type: 'bar', // 기본 차트 유형
-      data: {
-        labels: orderDate, // X축 (시간 데이터)
-        datasets: [
-          {
-            label: '판매 금액', // 첫 번째 데이터 세트
-            yAxisID : 'y1',
-            data: orderPrice, // Y축 값
-            backgroundColor: 'rgba(255, 99, 132, 0.5)', // 막대 색상
-            borderColor: 'rgba(255, 99, 132, 0.5)',
-            borderWidth: 1,
-            type: 'bar', // 막대형 차트
-          },
-          {
-            label: '발주 금액', // 첫 번째 데이터 세트
-            yAxisID : 'y1',
-            data: requestPrice, // Y축 값
-            backgroundColor: 'rgba(54, 162, 235, 0.5)', // 막대 색상
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1,
-            type: 'bar', // 막대형 차트
-          },
-          {
-            label: '판매 수량', // 두 번째 데이터 세트
-            yAxisID : 'y2',
-            data: orderCount, // Y축 값
-            borderColor: '#205375',
-            type: 'line', // 선형 차트
-            fill: false, // 선만 표시
-            tension: 0.4, // 곡선 정도
-          }
-        ]
-      },
-      options: {
-        scales: {
-          x: {
-            title: {
-                display: true,
-                text: '기간',
-            }
-        },
-        y1: {
-            type: 'linear',
-            position: 'left',
-            title: {
-                display: true,
-                text: '금액(원)',
-            }
-        },
-        y2: {
-            type: 'linear',
-            position: 'right',
-            title: {
-                display: true,
-                text: '수량(개)',
-            },
-            grid: {
-                drawOnChartArea: false, // Y2 그리드 비활성화
-            }
-          }
+        if (myChart != null) {
+          myChart.destroy();
         }
+        // forEach로 list의 값을 newArray에 추가
+        list.forEach(chart => {
+          orderDate.push(chart.orderDate);
+          orderCount.push(chart.orderCount);
+          orderPrice.push(chart.orderPrice);
+          requestPrice.push(chart.requestPrice);
+        });
+        console.log(orderDate);
+        console.log(orderCount);
+        console.log(orderPrice);
+        console.log(requestPrice);
+
+
+        myChart = new Chart(document.querySelector("#myChart").getContext('2d'), {
+          type: 'bar', // 기본 차트 유형
+          data: {
+            labels: orderDate, // X축 (시간 데이터)
+            datasets: [
+              {
+                label: '판매 금액', // 첫 번째 데이터 세트
+                yAxisID: 'y1',
+                data: orderPrice, // Y축 값
+                backgroundColor: 'rgba(255, 99, 132, 0.5)', // 막대 색상
+                borderColor: 'rgba(255, 99, 132, 0.5)',
+                borderWidth: 1,
+                type: 'bar', // 막대형 차트
+              },
+              {
+                label: '발주 금액', // 첫 번째 데이터 세트
+                yAxisID: 'y1',
+                data: requestPrice, // Y축 값
+                backgroundColor: 'rgba(54, 162, 235, 0.5)', // 막대 색상
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1,
+                type: 'bar', // 막대형 차트
+              },
+              {
+                label: '판매 수량', // 두 번째 데이터 세트
+                yAxisID: 'y2',
+                data: orderCount, // Y축 값
+                borderColor: '#205375',
+                type: 'line', // 선형 차트
+                fill: false, // 선만 표시
+                tension: 0.4, // 곡선 정도
+              }
+            ]
+          },
+          options: {
+            scales: {
+              x: {
+                title: {
+                  display: true,
+                  text: '기간',
+                }
+              },
+              y1: {
+                type: 'linear',
+                position: 'left',
+                title: {
+                  display: true,
+                  text: '금액(원)',
+                }
+              },
+              y2: {
+                type: 'linear',
+                position: 'right',
+                title: {
+                  display: true,
+                  text: '수량(개)',
+                },
+                grid: {
+                  drawOnChartArea: false, // Y2 그리드 비활성화
+                }
+              }
+            }
+          }
+        });
       }
-    });
-  }}) 
+    })
 
 }
